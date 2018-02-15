99100b3a068d0c5d1ffa1eb1ce712c9aac294c84
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-15 15:50:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text
 end
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/\d{10} \D /)
+    true
+  else
+    false 
+  end 
 end
